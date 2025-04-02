3accdbee4054b05c07f67f69c81debd570d5a29a
Junaid Adams <junaidadams117@gmail.com>
2025-04-02 10:21:23 +0200

    Changes to navigation bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,183 @@
+// Navbar.jsx
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import { navigationLinks } from "../../constants";
+import Toggle from "./Toggle";
+
+const variants = {
+  open: (height = 1000) => ({
+    clipPath: `circle(${height * 2 + 200}px at 40px 40px)`,
+    transition: {
+      type: "spring",
+      stiffness: 20,
+      restDelta: 2,
+    },
+  }),
+  closed: {
+    clipPath: "circle(30px at 40px 40px)",
+    transition: {
+      delay: 0.01,
+      type: "spring",
+      stiffness: 400,
+      damping: 40,
+    },
+  },
+};
+
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="flex bg-eggshell">
-      <ul className="space-x-8 capitalize flex flex-row ml-auto text-prussianBlue font-semibold text-lg">
+    <div className="z-20 flex justify-between items-center min-w-screen sticky top-0 min-h-[56px] px-4 backdrop-blur-lg">
+      {/* Navigation Links */}
+
+      <ul className="space-x-8 capitalize hidden md:flex flex-row p-2 mx-auto bg-periwinkle rounded-full">
         {navigationLinks.map((link) => (
-          <li key={link.key} className="p-2">
-            <a href={link.link}>{link.name}</a>
+          <li key={link.key} className="">
+            <a
+              href={link.link}
+              className="text-space-cadet  py-2 px-3 rounded-full hover:text-indigo-600 font-semibold"
+            >
+              {link.name}
+            </a>
           </li>
         ))}
+        {/* {!currentUser ? (
+          <li>
+            {" "}
+            <a
+              href="/login"
+              className="text-space-cadet  py-2 px-3 rounded-full hover:text-indigo-600 font-semibold"
+            >
+              Login
+            </a>
+          </li>
+        ) : (
+          ""
+        )}
+        {!currentUser ? (
+          <li>
+            {" "}
+            <a
+              href="/register"
+              className="text-space-cadet  py-2 px-3 rounded-full hover:text-indigo-600 font-semibold"
+            >
+              Register
+            </a>
+          </li>
+        ) : (
+          ""
+        )} */}
       </ul>
+
+      {/* Signed in menu desktop */}
+
+      {/* {currentUser ? (
+        <div className="z-20 hidden md:flex fixed flex-col mt-2">
+          <button
+            onClick={() => setMiniMenuIsOpen(!miniMenuIsOpen)}
+            className="shadow-lg bg-space-cadet py-2 px-3 rounded-full flex"
+          >
+            <div className="my-auto mr-2">
+              <BsChevronDoubleDown color="#e4d9ff" />
+            </div>
+            <span className="text-periwinkle">Hi, {currentUser.username}</span>
+          </button>
+          <AnimatePresence>
+            {miniMenuIsOpen && (
+              <motion.div
+                initial="closed"
+                animate="open"
+                exit="closed"
+                variants={variants}
+                transition={{ duration: 0.5 }}
+                className="absolute  bg-space-cadet shadow-lg rounded-t-3xl rounded-b-md flex-col flex"
+              >
+                <button
+                  onClick={() => setMiniMenuIsOpen(!miniMenuIsOpen)}
+                  className="py-2 px-3 rounded-full flex"
+                >
+                  <div className="my-auto mr-2">
+                    <BsChevronDoubleUp color="#e4d9ff" />
+                  </div>
+                  <span className="text-periwinkle">
+                    Hi, {currentUser.username}
+                  </span>
+                </button>
+                {signedInNavLinks.map((link) => (
+                  <a
+                    key={link.key}
+                    href={link.link}
+                    className="px-4 py-2 hover:bg-delft-blue capitalize text-ghost-white"
+                  >
+                    {link.name}
+                  </a>
+                ))}
+              </motion.div>
+            )}
+          </AnimatePresence>
+        </div>
+      ) : (
+        ""
+      )} */}
+
+      {/* Toggle Button */}
+
+      <div className="md:hidden ">
+        <Toggle isOpen={isOpen} onClick={() => setIsOpen(!isOpen)} />
+      </div>
+
+      {/* Mobile Menu */}
+
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            initial="closed"
+            animate="open"
+            exit="closed"
+            variants={variants}
+            transition={{ duration: 0.5 }}
+            className="absolute top-0 left-0 w-full bg-delft-blue shadow-lg flex flex-col space-y-6 px-4 pt-2 pb-10 capitalize md:hidden"
+          >
+            <div className="md:hidden ">
+              <Toggle isOpen={isOpen} onClick={() => setIsOpen(!isOpen)} />
+            </div>
+            {navigationLinks.map((link) => (
+              <a
+                key={link.key}
+                href={link.link}
+                className="text-ghost-white hover:text-periwinkle m-auto "
+                onClick={() => setIsOpen(false)} // Close menu on link click
+              >
+                {link.name}
+              </a>
+            ))}
+            {/* {currentUser ? (
+              <a
+                href="/profile-page"
+                className="text-ghost-white hover:text-periwinkle m-auto "
+              >
+                Profile
+              </a>
+            ) : (
+              <div className="m-auto flex space-y-6 flex-col">
+                <a
+                  href="/login"
+                  className="text-ghost-white hover:text-periwinkle m-auto "
+                >
+                  Login
+                </a>
+                <a
+                  href="/register"
+                  className="text-ghost-white hover:text-periwinkle m-auto "
+                >
+                  Register
+                </a>
+              </div>
+            )} */}
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,19 @@
 const Home = () => {
   return (
-    <div className="min-h-screen bg-eggshell flex">
-      <div className="m-auto space-y-6">
-        <h1 className="m-auto text-richBlack text-6xl font-black">
-          Welcome to Halaal Hub
+    <div className="min-h-screen -mt-[56px] bg-eggshell flex">
+      <div className="m-auto flex flex-col space-y-4">
+        <h1 className="m-auto text-center text-richBlack tracking-widest text-6xl font-black">
+          Welcome to
+          <br /> Halaal Hub
         </h1>
         <p className="tracking-[0.3em] text-xl font-light mx-auto w-fit">
           The Halaal food directory.
         </p>
         <a
           href="/get-started"
-          className="bg-prussianBlue text-white px-4 py-2 rounded-md mx-auto w-fit"
+          className="bg-prussianBlue text-white px-4 py-2 rounded-md m-auto w-fit"
         >
-          <button>Get Started!</button>
+          <button className="">Get Started!</button>
         </a>
       </div>
     </div>
