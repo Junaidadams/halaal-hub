22dcd0cb8c77772aaff4481165574321a5a46509
Junaid Adams <junaidadams117@gmail.com>
2025-06-08 12:52:05 +0200

    Changes to registration submit function

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,15 @@
 import { useState } from "react";
 import { accountTypes } from "../../constants";
+import apiRequest from "../../lib/apiRequest";
 
 const Register = () => {
+  const [isLoading, setIsloading] = useState({
+    formSubmit: false,
+    error: "",
+    success: false,
+    complete: false,
+  });
+
   const [formData, setFormData] = useState({
     username: "",
     role: "",
@@ -13,11 +21,47 @@ const Register = () => {
     setFormData({ ...formData, role: value });
   };
 
+  const handleSubmit = async (e) => {
+    // console.log(formData);
+    e.preventDefault();
+    setIsloading((prev) => ({
+      ...prev,
+      formSubmit: true,
+      success: false,
+    }));
+
+    try {
+      const res = await apiRequest.post("/auth/register", {
+        ...formData,
+      });
+      setIsloading((prev) => ({
+        ...prev,
+        success: true,
+      }));
+    } catch (err) {
+      console.error(err.response?.data?.message || "An error occured.");
+
+      setIsloading((prev) => ({
+        ...prev,
+        error:
+          "Failed to send registration request." +
+          " " +
+          err.response?.data?.message,
+      }));
+    } finally {
+      setIsloading((prev) => ({
+        ...prev,
+        formSubmit: false,
+        complete: true,
+      }));
+    }
+  };
+
   return (
     <div className="min-h-screen -mt-[56px] bg-eggshell flex">
       <div className="mx-auto my-20 min-h-screen w-[95%] flex flex-col">
         <div className="flex mx-auto md:my-10 lg:my-14 xl:my-20 w-full sm:w-2/3 md:w-1/2 xl:w-1/3 flex-col font-poppins bg-white p-5">
-          <form className="space-y-4">
+          <form onSubmit={handleSubmit} className="space-y-4">
             <div className="space-y-2">
               <h1 className="text-2xl font-black tracking-wider mb-4">
                 Create Account
